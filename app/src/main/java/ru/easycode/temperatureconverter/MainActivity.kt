c27bfb656e8378cc92720c4b4d7a0c9ab9654f46
Azamat <zamametshin@gmail.com>
2023-12-04 14:51:30 +0300

    +exp Image, Button, remember(mutableStateOf())

--- app/src/main/java/ru/easycode/temperatureconverter/MainActivity.kt
+++ app/src/main/java/ru/easycode/temperatureconverter/MainActivity.kt
@@ -3,38 +3,68 @@ package ru.easycode.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import ru.easycode.temperatureconverter.ui.theme.TemperatureConverterTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                Hello("friend")
-                Hello("everyone")
-            }
+            MainActivityContent()
         }
     }
 }
 
 @Composable
-fun Hello(name: String) {
-    Text( "Hello $name! Welcome to Jetpack Compose.")
+fun Header(image: Int, description: String) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = description,
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
 }
+
+@Composable
+fun TemperatureText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble() * 9 / 5) + 32
+    Text(text = "$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text(text = "Convert")
+    }
+}
+
+@Composable
+fun MainActivityContent() {
+    val celsius = remember { mutableStateOf(0) }
+    Column {
+        Header(R.drawable.sunrise, "sunrise image")
+        ConvertButton { celsius.value = 20 }
+        TemperatureText(celsius = 0)
+    }
+}
+
+
 @Preview(showBackground =  true)
 @Composable
 fun PreviewMainActivity() {
-    Column {
-        Hello(name = "friend")
-        Hello(name = "everyone")
-    }
+    MainActivityContent()
 }
